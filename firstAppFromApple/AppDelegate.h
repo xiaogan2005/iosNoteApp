4079d4281610bc2d3a97ca2e5dae4eb4752e8482
Rui Guo <rey.goal.2013@gmail.com>
2015-09-29 13:43:41 -0400

    Initial Commit

---
+++ firstAppFromApple/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  firstAppFromApple
+//
+//  Created by GuoRui on 9/29/15.
+//  Copyright © 2015 GuoRui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ firstAppFromApple/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  firstAppFromApple
+//
+//  Created by GuoRui on 9/29/15.
+//  Copyright © 2015 GuoRui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
